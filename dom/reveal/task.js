61b5ff9732b2fd27db65260718509609ae5f791e
gambino <e.set2283@gmail.com>
2025-08-21 22:55:55 +0300

    first commit reveal

---
+++ dom/reveal/task.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const reveals = document.querySelectorAll(".reveal");
+
+  function checkVisibility() {
+    reveals.forEach(el => {
+      const rect = el.getBoundingClientRect();
+
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        el.classList.add("reveal_active");
+      } else {
+        el.classList.remove("reveal_active");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", checkVisibility);
+  checkVisibility();
+});
